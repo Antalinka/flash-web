a9dfa8dd6e3fdb35a743ff4c6d62d45f09201305
Evgeniya <yantalinka@gmail.com>
2023-03-08 10:54:49 +0000

    redirect to appstore or appplication

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 const apps = {
-    // Android: 'https://play.google.com/',
     iOS: 'http://itunes.apple.com/app/id1485265975',
   };
   
@@ -11,110 +10,41 @@ const apps = {
     return null;
   };
   
+    
+  var timeout;
+  function preventPopup() {
+    clearTimeout(timeout);
+    timeout = null;
+    window.removeEventListener('pagehide', preventPopup);
+  }
+
   const redirect = () => {
     let os = platform();
     if (os in apps) {
-    //   location.replace(apps[os]);
-    // setTimeout(function () { window.location = "http://itunes.apple.com/app/id1485265975"; }, 25);
-
     const params = new URLSearchParams(window.location.search)
-    // print("text")
-    // print(params)
     let setID = params.get("setID");
 
-    const message = document.querySelector('.message');
-    message.innerText = setID;
-
-    // console.log('params');
-    // console.log(params);
-
     // let folderID = params.get("folderID").toString()
-    window.location = "com.flashmemory://sharing?folderID=" + setID
+    // const url =  "com.flashmemory://sharing?folderID=" + setID
+    // window.open(url);
+    // window.location.href = url; 
+
+    // const message = document.querySelector('.message');
+    // message.innerText = "Redirect to application" + setID;
+
+    var appUrl =  "com.flashmemory://sharing?folderID=" + setID;
+    window.location = appUrl;
+
+    timeout = setTimeout(function(){
+      if(confirm('You do not seem to have the App installed, do you want to go download it now?')){
+        document.location = apps[os];
+      }
+    }, 1000);
+    window.addEventListener('pagehide', preventPopup);
     } else {
       const message = document.querySelector('.message');
       message.innerText = 'Oh, no! Open link on your iPhone';
     }
   };
-  
+
   redirect();
-
-
-// function addOrUpdateURLParam(key, value) {
-//     const searchParams = new URLSearchParams(window.location.search)
-//     searchParams.set(key, value)
-
-//     const newRelativePathQuery = window.location.pathname + "?" + searchParams.toString()
-
-// }
-
-// addOrUpdateURLParam("name", "Jane")
-
-
-
-/*
-
-let routes = {};
-let templates = {};
-
-let app_div = document.getElementById('app');
-
-function home() {
-    let div = document.createElement('div');
-    let link = document.createElement('a');
-    link.href = '#/about';
-    link.innerText = 'About';
-
-    div.innerHTML = '<h1>Home</h1>';
-    div.appendChild(link);
-
-    app_div.appendChild(div);
-};
-
-function about() {
-    let div = document.createElement('div');
-    let link = document.createElement('a');
-    link.href = '#/';
-    link.innerText = 'Home';
-
-    div.innerHTML = '<h1>About</h1>';
-    div.appendChild(link);
-
-    app_div.appendChild(div);
-};
-
-function route (path, template) {
-  if (typeof template === 'function') {
-      return routes[path] = template;
-  }
-  else if (typeof template === 'string') {
-      return routes[path] = templates[template];
-  } else {
-      return;
-  };
-};
-
-function template (name, templateFunction) {
-  return templates[name] = templateFunction;
-};
-
-template('home', function(){
-  home();
-});
-
-template('about', function(){
-  about();
-});
-
-route('/', 'home');
-route('/about', 'about');
-
-function router(evt) {
-  let url = window.location.hash.slice(1) || '/';
-  let route = resolveRoute(url);
-
-  route();
-};
-
-window.addEventListener('load', router);
-window.addEventListener('hashchange', router);
-*/
